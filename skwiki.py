e95dbf7eb9a6199d1155550749bd6225000782cb
Haato <35552782+Haato3o@users.noreply.github.com>
2018-11-15 15:15:43 -0200

    some changes

--- skwiki.py
+++ skwiki.py
@@ -1,103 +1,78 @@
-'''
-Spiral Knights Wiki Parser made by Haato
-                How to use
-import skwiki
-skwiki.item_description('item name')
-returns the <item name> description
+import requests
+from bs4 import BeautifulSoup
+wikiUrl = 'http://wiki.spiralknights.com/'
+mediaUrl = 'http://media3.spiralknights.com/wiki-'
+request = requests.Session()
 
-skwiki.item_status('item name')
-returns the <item name> status
-
-skwiki.item_image('item name')
-returns the <item name> image
-
-skwiki.item_tier('item name')
-returns how many stars the <item name> has
-
-if any of the functions return False, then the wiki is either offline or the item name is invalid.
-
-'''
-import urllib
-import urllib.request
-try:
-	from bs4 import BeautifulSoup
-except:
-	print('BeautifulSoup required, download it before running the script.')
-
-def item_description(ITEM_NAME: str):
-		ITEM_NAME = ITEM_NAME.replace(" ", "_")
-		URL = 'http://wiki.spiralknights.com/%s' % ITEM_NAME.title()
+class item:
+	'''
+		Searches Spiral Knights wiki for the desired item.
+	'''
+	def __init__(self, name):
+		'''
+			:name: Item name
+		'''
+		self.name = name.replace(' ', '_').title()
+	
+	def description(self):
+		'''
+			Function to get item description
+			:name: Item name
+			:return: String
+		'''
 		try:
-			URL = urllib.request.urlopen(URL).read()
-			URL = URL.decode('utf-8')
-			SOUP_HTML_PARSER = BeautifulSoup(URL, 'html.parser')
-			try:
-				SOUP_HTML_PARSER.find(alt='stats')
-				ITEM_DESCRIPTION = SOUP_HTML_PARSER.find(id='Description')
-				ITEM_DESCRIPTION = ITEM_DESCRIPTION.find_next('p')
-				ITEM_DESCRIPTION = ITEM_DESCRIPTION.get_text()
-				return ITEM_DESCRIPTION
-			except:
-				return False
-		except:
-			return False
-
-def item_image(ITEM_NAME: str):
-	ITEM_NAME = ITEM_NAME.replace(" ", "_")
-	URL = 'http://wiki.spiralknights.com/%s' % ITEM_NAME.title()
-	try:
-		URL = urllib.request.urlopen(URL).read()
-		URL = URL.decode('utf-8')
-		SOUP_HTML_PARSER = BeautifulSoup(URL, 'html.parser')
+			content = request.get(wikiUrl+self.name).text
+			htmlParser = BeautifulSoup(content, 'html.parser')
+			htmlParser.find(alt='stats')
+			description = htmlParser.find(id='Description').find_next('p').get_text()
+			return description
+		except Exception:
+			print(Exception)
+			print('No item found.')
+	
+	def image(self):
+		'''
+			Function to get item image
+			:name: Item name
+			:return: String
+		'''
 		try:
-			SOUP_HTML_PARSER.find(alt='stats')
-			ITEM_IMAGE = SOUP_HTML_PARSER.find('img')
-			ITEM_IMAGE = list(str(ITEM_IMAGE.get('src')))
-			ITEM_IMAGE.remove('/')
-			ITEM_IMAGE = "".join(ITEM_IMAGE)
-			ITEM_IMAGE_URL = 'http://media3.spiralknights.com/wiki-%s' % ITEM_IMAGE
-			return ITEM_IMAGE_URL
+			content = request.get(wikiUrl+self.name).text
+			htmlParser = BeautifulSoup(content, 'html.parser')
+			htmlParser.find(alt='stats')
+			image = list(str(htmlParser.find('img').get('src')))
+			image.remove('/')
+			return '{}{}'.format(mediaUrl, ''.join(image))
 		except:
-			return False
-	except:
-		return False
-
-def item_status(ITEM_NAME: str):
-	ITEM_NAME = ITEM_NAME.replace(" ", "_")
-	URL = 'http://wiki.spiralknights.com/%s' % ITEM_NAME.title()
-	try:
-		URL = urllib.request.urlopen(URL).read()
-		URL = URL.decode('utf-8')
-		SOUP_HTML_PARSER = BeautifulSoup(URL, 'html.parser')
+			print('No item found')
+	
+	def status(self):
+		'''
+			Function to get item status
+			:name: Item name
+			:return: String
+		'''
 		try:
-			ITEM_STATUS = SOUP_HTML_PARSER.find(alt='stats')
-			ITEM_STATUS = list(str(ITEM_STATUS.get('src')))
-			ITEM_STATUS.remove('/')
-			ITEM_STATUS = "".join(ITEM_STATUS)
-			ITEM_STATUS_URL = 'http://media3.spiralknights.com/wiki-%s' % ITEM_STATUS
-			return ITEM_STATUS_URL
+			content = request.get(wikiUrl+self.name).text
+			htmlParser = BeautifulSoup(content, 'html.parser')
+			status = list(str(htmlParser.find(alt='stats').get('src')))
+			status.remove('/')
+			return '{}{}'.format(mediaUrl, ''.join(image))
 		except:
-			return False
-	except:
-		return False
+			print('No item found')
 
-def item_tier(ITEM_NAME: str):
-	ITEM_NAME = ITEM_NAME.replace(" ", "_")
-	URL = 'http://wiki.spiralknights.com/%s' % ITEM_NAME.title()
-	try:
-		URL = urllib.request.urlopen(URL).read()
-		URL = URL.decode('utf-8')
-		SOUP_HTML_PARSER = BeautifulSoup(URL, 'html.parser')
+	def tier(self):
+		'''
+			Function to get item tier
+			:name: Item name
+			:return: String
+		'''
 		try:
-			SOUP_HTML_PARSER.find(alt='stats')
-			ITEM_TIER = SOUP_HTML_PARSER.find('td')
-			ITEM_TIER = ITEM_TIER.find_all_next('td')
-			ITEM_TIER = ITEM_TIER[3].get_text()
-			ITEM_TIER = list(ITEM_TIER)
-			ITEM_TIER.remove(ITEM_TIER[6])
-			ITEM_TIER = "".join(ITEM_TIER)
-			return ITEM_TIER
+			content = request.get(wikiUrl+self.name).text
+			htmlParser = BeautifulSoup(content, 'html.parser')
+			htmlParser.find(alt='stats')
+			tier = list(htmlParser.find('td').find_all_next('td')[3].get_text())
+			tier.remove(item[6])
+			return ''.join(tier)
 		except:
-			return False
-	except:
-		return False
+			print('No item found')
